1c772d5c5c80a04921b4fa930cfcdc060c60f4f2
THBD7 <thibaudmichon7@gmail.com>
2025-10-10 09:04:45 +0200

    change color, design to look less old

--- shopScript.js
+++ shopScript.js
@@ -21,4 +21,50 @@ sliders.forEach(slider => {
       e.preventDefault();
     }
   });
+
+  let isDown = false;
+  let startX;
+  let scrollLeft;
+
+  const disableSnap = () => {
+    track.dataset.snap = track.style.scrollSnapType;
+    track.style.scrollSnapType = "none";
+  };
+
+  const enableSnap = () => {
+    if (track.dataset.snap) {
+      track.style.scrollSnapType = track.dataset.snap;
+    } else {
+      track.style.scrollSnapType = "";
+    }
+  };
+
+  track.addEventListener("mousedown", (e) => {
+    isDown = true;
+    disableSnap();
+    track.classList.add("dragging");
+    startX = e.pageX - track.offsetLeft;
+    scrollLeft = track.scrollLeft;
+    e.preventDefault();
+  });
+
+  track.addEventListener("mouseleave", () => {
+    if (isDown) enableSnap();
+    isDown = false;
+    track.classList.remove("dragging");
+  });
+
+  track.addEventListener("mouseup", () => {
+    enableSnap();
+    isDown = false;
+    track.classList.remove("dragging");
+  });
+
+  track.addEventListener("mousemove", (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - track.offsetLeft;
+    const walk = (x - startX) * 1.2;
+    track.scrollLeft = scrollLeft - walk;
+  });
 });
